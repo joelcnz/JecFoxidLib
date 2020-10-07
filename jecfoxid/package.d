3fe21cf7b10561d98d4809f7d7dba335be08e35d
Joel Christensen <joelcnz@gmail.com>
2020-10-07 21:58:58 +1300

    Init commit

---
+++ jecfoxid/package.d
@@ -0,0 +1,16 @@
+module jecfoxid;
+
+public import
+	jecfoxid.base,
+	jecfoxid.draw,
+	jecfoxid.gui,
+	jecfoxid.guiconfirm,
+	jecfoxid.guifile,
+	jecfoxid.input,
+	jecfoxid.jexting,
+	jecfoxid.letter,
+	jecfoxid.lettermanager,
+	jecfoxid.sdl_gfx_primitives,
+	jecfoxid.setup,
+	foxid,
+	foxid.sdl;
